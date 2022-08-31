d9841a91faf6fd15aa4487e3de9d0208ebb38c4e
yechan <noy3928@naver.com>
2022-08-31 17:02:45 +0900

    variable in java

---
+++ JAVA/variable.java
@@ -0,0 +1,26 @@
+public class Creator {
+	public static void main(String[] args) {
+        String name = "James Gosling";
+        int yearCreated = 1995;
+        System.out.println(name);
+        System.out.println(yearCreated);
+
+
+        double androidShare = 81.7;
+        System.out.println(androidShare);
+
+        boolean intsCanHoldDecimals = false;
+        System.out.println(intsCanHoldDecimals);
+
+        char expectedGrade = 'A';
+        System.out.println(expectedGrade);  
+
+        String openingLyrics = "Yesterday, all my troubles seemed so far away";
+        System.out.println(openingLyrics);
+
+        String name = "노예찬";
+        int age = 28;
+        double desiredSalary = 10000000000.00;
+        char gender = 'm';
+        boolean lookingForJob = false;
+}
